81d0fcbdb06b4a2ca367091019b5202ca326e3d9
Truberton <gert-andry.kaaramees@tptlive.ee>
2018-02-08 08:39:22 +0200

    Failid lisatud

---
+++ Kontor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Kontor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Tuba = Console.ReadLine();
+
+            switch (Tuba.ToLower())
+            {
+                case "peauks":
+                    break;
+                case "arendajate ruum":
+                    break;
+                case "ülemuste ruum":
+                    break;
+                case "köök":
+                    break;
+                case "andmetöötlejate ruum":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
