4b4965337ce78a5f48494a196cd0b86708d38508
Truberton <gert-andry.kaaramees@tptlive.ee>
2018-02-08 09:46:41 +0200

    väike viga

--- Kontor/Koristaja.cs
+++ Kontor/Koristaja.cs
@@ -3,7 +3,7 @@
     public class Koristaja
     {
         public bool PeaUks_Access = true;
-        public bool ArendajateRuum_Access = false;
+        public bool ArendajateRuum_Access = true;
         public bool ÜlemusteRuum_Access = false;
         public bool Köök_Access = true;
         public bool AndmetöötlejateRuum_Access = true;
