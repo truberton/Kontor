e04db9002df15254333fca4a9f68a1eb0b077281
Truberton <gert-andry.kaaramees@tptlive.ee>
2018-02-08 09:44:06 +0200

    Lisasin commenti

--- Kontor/Arendaja.cs
+++ Kontor/Arendaja.cs
@@ -2,7 +2,7 @@
 {
     public class Arendaja
     {
-        public bool PeaUks_Access;
+        public bool PeaUks_Access; //Protected ei saa millegi pärast kasutada, crashib...
         public bool ArendajateRuum_Access;
         public bool ÜlemusteRuum_Access;
         public bool Köök_Access;
